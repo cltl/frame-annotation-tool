56ddf6e3d45fa14bd2f3125e697f86955de57fa5
Filip Ilievski <filip.dbrsk@gmail.com>
2019-10-24 15:02:44 +0200

    create indices for better selection

--- scripts/create_indices.py
+++ scripts/create_indices.py
@@ -7,9 +7,15 @@ bin_dir='../data'
 doc_dir='../data/naf'
 inc2doc_file='%s/inc2doc_index.json' % bin_dir
 inc2str_file='%s/inc2str_index.json' % bin_dir
+proj2inc_file='%s/proj2inc_index.json' % bin_dir
+type2inc_file='%s/type2inc_index.json' % bin_dir
 
 inc2doc={}
 inc2str={}
+proj2inc=defaultdict(set)
+type2inc=defaultdict(set)
+
+project='pilot'
 
 for f in glob.glob('%s/*.bin' % bin_dir):
     with open(f, 'rb') as fl:
@@ -23,14 +29,25 @@ for f in glob.glob('%s/*.bin' % bin_dir):
         for rt in inc.reference_texts:
             rt_info='%s/%s' % (rt.language, rt.name)
             rts.append(rt_info)
-        key='%s/%s' % (inc.incident_type, inc.wdt_id)
+        key=inc.wdt_id
         inc2doc[key]=rts
         inc2str[key]=str_data
+        proj2inc[project].add(key)
+        type2inc[inc.incident_type].add(key)
 
-print(inc2doc)
-print(inc2str)
+
+new_t2i={}
+for k,v in type2inc.items():
+    new_t2i[k]=sorted(list(v))
+new_p2i={}
+for k,v in proj2inc.items():
+    new_p2i[k]=sorted(list(v))
 
 with open(inc2doc_file, 'w') as f:
     json.dump(inc2doc, f)
 with open(inc2str_file, 'w') as f:
     json.dump(inc2str, f)
+with open(proj2inc_file, 'w') as f:
+    json.dump(new_p2i, f)
+with open(type2inc_file, 'w') as f:
+    json.dump(new_t2i, f)
